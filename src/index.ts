e01ef1a13de33024c057d701ed896f4c30934117
nogahaskal <noghas12@gmail.com>
2020-09-13 11:06:03 +0300

    refactor: use socket npm package

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
-import { Socket } from './socket';
-import { RabbitHandler } from './utils/rabbitConnector';
+import * as socketRabbit from 'socket-rabbit';
+import { config } from './config';
+
+export interface IMessage {
+  operation: OPERATION;
+  objectType: OBJECTTYPE;
+  fileID: string;
+  folderID: string;
+  userIDs: string[];
+}
+
+export enum OBJECTTYPE {
+  FILE = 'FILE',
+  PERMISSION = 'PERMISSION'
+}
+
+enum OPERATION {
+    ADD = 'ADD',
+    UPDATE = 'UPDATE',
+    DELETE= 'DELETE',
+}
 
 (async () => {
-  Socket.startSocket();
-  await RabbitHandler.initalizeRabbit();
+  await socketRabbit.connect(config.rabbit.connectionString, config.rabbit.queue);
+  await socketRabbit.listen(config.rabbit.queue, (content) => {
+    const message = JSON.parse(content);
+
+    const data = { fileID: message.fileID, operation: message.operation, folderID: message.folderID };
+    const rooms = message.userIDs;
+    const event = message.objectType;
+
+    const msg = {
+      data,
+      rooms,
+      event
+    };
+
+    return msg;
+  });
 })();
