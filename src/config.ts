aab741cdd8dc0a52783889d0f9677212113c0b2d
nogahaskal <nogahaskal@gmail.com>
2020-09-23 11:00:52 +0300

    feat: add logger

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
+const esHost: string = process.env.ELASTICSEARCH_URL || 'http://localhost:9200';
+const esUser: string = process.env.ELASTICSEARCH_USER || '';
+const esPass: string = process.env.ELASTICSEARCH_PASSWORD || '';
+
 export const config = {
   appPort: process.env.PORT || 8080,
   socket: {
@@ -14,5 +18,15 @@ export const config = {
   authorization: {
     secret: process.env.GW_SECRET || '',
     origin: process.env.GW_WEB_UI || 'localhost',
+  },
+  logger: {
+    serviceName: 'socket-service',
+    options: {
+      hosts: esHost && esHost.split(','),
+      httpAuth: `${esUser}:${esPass}`,
+    },
+    indexPrefix: process.env.LOG_INDEX || 'kdrive',
+    debugMode: process.env.DEBUG_MODE === 'true',
+    useElastic: process.env.ELASTICSEARCH_URL !== undefined,
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import * as socketRabbit from "rabbit-socket.io";
-import * as http from "http";
-import express from "express";
-import { router } from "./router";
-import { config } from "./config";
+import * as socketRabbit from 'rabbit-socket.io';
+import * as http from 'http';
+import express from 'express';
+import { log, Severity } from './logger';
+import { router } from './router';
+import { config } from './config';
 
 export interface IMessage {
-  operation: operation;
-  objectType: objectType;
+  operation: Operation;
+  objectType: ObjectType;
   fileID: string;
   folderID: string;
   userIDs: string[];
 }
 
-export enum objectType {
-  FILE = "FILE",
-  PERMISSION = "PERMISSION",
+export enum ObjectType {
+  FILE = 'FILE',
+  PERMISSION = 'PERMISSION',
 }
 
-enum operation {
-  ADD = "ADD",
-  UPDATE = "UPDATE",
-  DELETE = "DELETE",
+enum Operation {
+  ADD = 'ADD',
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE',
 }
 
 /**
@@ -32,7 +33,7 @@ const startServer = async (port: number) => {
   app.use(router);
 
   http.createServer(app).listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
+    log(Severity.INFO, `Express server listening on port ${port}`, 'initApp');
   });
 };
 
@@ -40,43 +41,55 @@ const startServer = async (port: number) => {
  * connectRabbitSocket is connecting to rabbit with the connection string and the port of the socket
  */
 const connectRabbitSocket = async () => {
-  await socketRabbit.connect(
-    config.rabbit.connectionString,
-    config.socket.port as number,
-    {
-      useRedisAdapter: true,
-      redisHost: config.redis.host,
-      redisPort: config.redis.port as number,
-    }
-  );
+  try {
+    await socketRabbit.connect(
+      config.rabbit.connectionString,
+      config.socket.port as number,
+      {
+        useRedisAdapter: true,
+        redisHost: config.redis.host,
+        redisPort: config.redis.port as number,
+      }
+    );
+  } catch (err) {
+    log(Severity.ERROR, `error while connecting to socketRabbit : ${err}`, 'socketRabbit.connect');
+    return;
+  }
+
 };
 
 /***
  * listenToQueue is listens to the queue, and formats all the recived messages to the require type.
  */
 const listenToQueue = async () => {
-  await socketRabbit.listen(config.rabbit.queue, (content: any) => {
-    const message = JSON.parse(content);
+  try {
+    await socketRabbit.listen(config.rabbit.queue, (content: any) => {
+      const message = JSON.parse(content);
 
-    const data: { fileID: string; folderID: string } = {
-      fileID: message.fileID,
-      folderID: message.folderID,
-    };
-    const rooms: string[] = message.userIDs;
-    const event: string = `${message.objectType}_${message.operation}`;
+      const data: { fileID: string; folderID: string } = {
+        fileID: message.fileID,
+        folderID: message.folderID,
+      };
+      const rooms: string[] = message.userIDs;
+      const event: string = `${message.objectType}_${message.operation}`;
 
-    const msg: socketRabbit.Message = {
-      data,
-      rooms,
-      event,
-    };
+      const msg: socketRabbit.Message = {
+        data,
+        rooms,
+        event,
+      };
 
-    return msg;
-  });
+      return msg;
+    });
+  } catch (err) {
+    log(Severity.ERROR, `error while listenning to the queue : ${err}`, 'socketRabbit.listen');
+    return;
+  }
 };
 
 (async () => {
   await startServer(config.appPort as number);
+
   await connectRabbitSocket();
   await listenToQueue();
 })();
